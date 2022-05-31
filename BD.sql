22a2e9513de177e7c3c543c3123aec8aef8a4944
Isamar19 <xiomaraloaiza18@gmail.com>
2022-05-31 00:07:15 -0500

    se agregan validaciones

--- BD.sql
+++ BD.sql
@@ -1,27 +1,36 @@
--- Creación de la base de datos
-CREATE DATABASE Laboratorio
+-- Creación de la base de datos si no existe
+IF (NOT EXISTS (SELECT name FROM master.sys.databases WHERE ('[' + name + ']' = 'Laboratorio' OR name = 'Laboratorio')))
+BEGIN
+	CREATE DATABASE Laboratorio
+END
 GO
 
 -- Creación de la tabla Medicamentos
-CREATE TABLE Medicamentos (
+IF NOT EXISTS (SELECT * FROM INFORMATION_SCHEMA.TABLES WHERE TABLE_NAME = N'Medicamentos')
+BEGIN
+  CREATE TABLE Medicamentos (
 	Id INT IDENTITY PRIMARY KEY,
 	Nombre VARCHAR(100),
 	Composicion VARCHAR(10),
 	FormaFarmaceutica VARCHAR(50)	
 )
+END
 GO
 
 -- Creación de la tabla DatosClinicos
-CREATE TABLE DatosClinicos (
-	Id INT IDENTITY PRIMARY KEY,
-	Posologia VARCHAR(MAX),
-	Indicaciones VARCHAR(MAX),
-	Contraindicaciones VARCHAR(MAX),
-	Precauciones VARCHAR(MAX),
-	MedicamentoId INT,
+IF NOT EXISTS (SELECT * FROM INFORMATION_SCHEMA.TABLES WHERE TABLE_NAME = N'DatosClinicos')
+BEGIN
+	CREATE TABLE DatosClinicos (
+		Id INT IDENTITY PRIMARY KEY,
+		Posologia VARCHAR(MAX),
+		Indicaciones VARCHAR(MAX),
+		Contraindicaciones VARCHAR(MAX),
+		Precauciones VARCHAR(MAX),
+		MedicamentoId INT,
 	
-	FOREIGN KEY (MedicamentoId) REFERENCES Medicamentos(Id)
-)
+		FOREIGN KEY (MedicamentoId) REFERENCES Medicamentos(Id)
+	)
+END
 GO
 
 -- Insertamos información a la tabla Medicamentos
